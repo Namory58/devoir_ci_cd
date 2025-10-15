4317713da81a5f9abc9ccc95ffc878d024ee8c7f
Namory <namory@Admins-MacBook-Air.local>
2025-10-15 11:33:41 +0200

    correction d'affichage de la page

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,3 +5,5 @@ test ("La fonction retourne une citation", () => {
     expect(typeof result).toBe("string");
     expect(result.length).toBeGreaterThan(0);
 }) ;
+
+// Pour exécuter ce test, utilisez la commande : npx jest tests/app.test.js
